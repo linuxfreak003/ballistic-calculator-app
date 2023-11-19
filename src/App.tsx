71492d0476a48cd82c1d8eefbd84151f5c7891be
Jared Chapman <jared.chapman@tcnbroadcasting.com>
2023-11-18 21:22:01 -0700

    added load, and trying CreateLoad

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
 import './App.css';
+import {
+  CreateLoadRequest,
+  CreateLoadResponse,
+  DragFunction,
+} from './Entities';
+
+async function CreateLoad(request: CreateLoadRequest) {
+  const url = 'http://localhost:8080/ballistic/createload'
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+    console.log(response);
+  } catch (error) {
+    console.error("Error:", error)
+  }
+}
 
 export default function App() {
+  CreateLoad({
+    load: {
+      loadId: 0,
+      bullet: {
+        caliber: .338,
+        weight: 285,
+        bc: {
+          value: .356,
+          dragFunction: DragFunction.DRAG_FUNCTION_G7,
+        },
+        length: 1.5,
+      },
+      muzzleVelocity: 2950,
+    },
+  })
+
   return (
     <div>
       <main>
